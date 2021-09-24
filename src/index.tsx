c883207371081040fbcec4a6e5c5353706de1287
mia02125 <mia02125@naver.com>
2021-09-24 18:13:21 +0900

    초기 세팅

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import Left from './map/Left';
+import Right from './map/Right';
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <div className={'main-container'}>
+      <Left />
+      <Right />
+    </div>
   </React.StrictMode>,
   document.getElementById('root')
 );
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
---
+++ src/map/Left.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import '../css/main-page.css';
+
+function App() {
+ return (
+    <div className="main-item1">
+        asd
+    </div>
+ )
+}
+
+export default App;
